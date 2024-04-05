dcd4b5194fda9038122a7c8a3b80c7cdb1bf2a86
pysrxyz <gm@pysr.xyz>
2024-04-05 23:42:11 +0700

    style: layout of hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,33 @@ import Scene from "@/components/Scene";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="relative flex flex-col-reverse md:flex-row place-items-center min-h-[100vh] w-full px-3 md:px-0">
-        <div className="w-full md:w-3/5 h-full text-4xl md:text-8xl font-bold text-white md:px-10 flex flex-col items-center justify-center">
-          <div className="w-full">
-            Have fun & earn tokens
-            <br />
-            with Chess-on-chain
-          </div>
-          <div className="text-base md:text-lg font-light text-justify text-white mt-5 leading-relaxed">
-            Challenge other wallets to play against you or play against an AI.
-            Earn points by playing and winning. Complete recurring quests and
-            collect achievements!
+    <main className="w-full h-screen py-4">
+      <div className="w-full h-full relative flex flex-col items-center justify-between">
+        <div className="w-full py-4 text-4xl md:text-8xl font-bold text-white md:px-10 flex flex-col items-center justify-between">
+          <div>
+            <div className="w-full text-center">
+              Have fun & earn tokens
+              <br />
+              with Chess-on-chain
+            </div>
+            <div className="text-base md:text-lg font-light text-justify text-white mt-5 leading-relaxed">
+              Challenge other wallets to play against you or play against an AI.
+              Earn points by playing and winning. Complete recurring quests and
+              collect achievements!
+            </div>
           </div>
         </div>
-        <div className="w-full md:w-2/5 h-[450px] md:min-h-[100vh]">
+
+        <div className="text-white">
+          <button
+            // Big CTA  button White bg transparent text
+            className="w-80 h-12 md:h-16 bg-white text-primary font-bold text-lg md:text-2xl rounded-2xl border border-white"
+          >
+            Play Now
+          </button>
+        </div>
+
+        <div className="w-full h-full absolute -z-10">
           <Scene />
         </div>
       </div>
