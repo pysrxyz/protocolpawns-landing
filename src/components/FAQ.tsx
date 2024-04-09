1a4b9d47641d991f3d24b99bca5000e63eb5798b
pysrxyz <gm@pysr.xyz>
2024-04-09 16:38:04 +0700

    use details component for FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import { MAIN_CONTENT } from "@/content";
-import { Disclosure } from "@headlessui/react";
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
 
 export default function FAQ() {
   return (
@@ -13,45 +9,41 @@ export default function FAQ() {
           <h2 className="text-white text-3xl md:text-5xl font-bold my-4">
             {MAIN_CONTENT.FAQ.subtitle}
           </h2>
-          <dl className="mt-10 space-y-12">
+
+          <div className="mt-10 space-y-12">
             {MAIN_CONTENT.FAQ.questions.map((faq) => (
-              <Disclosure
-                as="div"
+              <details
                 key={faq.question}
-                className="bg-neutral-800 rounded-lg hover:bg-neutral-600"
+                className="bg-neutral-800 rounded-lg hover:bg-neutral-600 cursor-pointer [&_svg]:open:-rotate-180 list-none"
               >
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className={`w-full px-6 pt-4 md:py-8`}>
-                      <dt className="flex w-full items-start justify-between text-left text-white">
-                        <span className="text-lg font-semibold leading-7">
-                          {faq.question}
-                        </span>
-                        <span className="ml-6 flex h-7 items-center">
-                          {open ? (
-                            <ChevronUpIcon
-                              className="h-6 w-"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <ChevronDownIcon
-                              className="h-6 w-6"
-                              aria-hidden="true"
-                            />
-                          )}
-                        </span>
-                      </dt>
-                    </Disclosure.Button>
-                    <Disclosure.Panel as="dd" className="pr-12 pb-6 px-6">
-                      <p className="text-base leading-7 text-gray-300">
-                        {faq.answer}
-                      </p>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
+                <summary className="w-full px-6 py-4 md:py-8 list-none marker:hidden">
+                  <div className="flex w-full items-start justify-between text-left text-white">
+                    <span className="text-lg font-semibold leading-7 text-white">
+                      {faq.question}
+                    </span>
+                    <div className="ml-6 flex h-7 items-center">
+                      <svg
+                        className="rotate-0 transform transition-all duration-300"
+                        fill="none"
+                        height="20"
+                        width="20"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        viewBox="0 0 24 24"
+                      >
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                      </svg>
+                    </div>
+                  </div>
+                </summary>
+                <p className="pr-12 pb-6 px-6 text-base leading-7 text-gray-300">
+                  {faq.answer}
+                </p>
+              </details>
             ))}
-          </dl>
+          </div>
         </div>
       </div>
     </section>
